a5850eb2e8c54539f3174708a9e5bb2589c7c57e
Jules White <jules.white@gmail.com>
2015-08-19 08:23:49 -0500

    - Refactored testing into its own file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
-
+var expectThat = require('./lib/test/expect').expectThat;
 
 
 
@@ -91,36 +91,7 @@ function getDirectoryInfo(path){
     }
 }
 
-var testNumber = 1;
-function expectThat(test){
-    var check = test.check;
-    var expectedValue = test.expectedValue;
-    var msg = test.msg;
 
-    var exception = null;
-    var actualValue = null;
-
-    try{
-        actualValue = check();
-    }catch(ex){
-        exception = ex;
-    }
-
-    if(!exception && (expectedValue === actualValue)){
-        console.log("Test "+testNumber+" passed");
-    }
-    else if(exception){
-        console.log("Test "+testNumber+" failed");
-        console.log("At: "+exception.stack);
-        console.log("[\n\t"+msg+"\n]");
-    }
-    else {
-        console.log("Test "+testNumber+" failed");
-        console.log("[\n\t"+msg+"\n]");
-    }
-
-    testNumber++;
-}
 
 var directoryState1 = getDirectoryInfo("./test-data/folder1");
 var directoryState2 = getDirectoryInfo("./test-data/folder2");
