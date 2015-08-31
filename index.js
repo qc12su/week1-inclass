b6297d00e57a55ba3f679a1335802e57bbc90f88
Jules White <jules.white@gmail.com>
2015-08-30 20:52:52 -0500

    - Refactored code to use an asynchronous event-driven based file handler   API - Added support for remote sync via dnode

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var _ = require('lodash');
 var fs = require('fs');
 
 var sync = require('./lib/sync/sync');
+var dnodeClient = require("./lib/sync/sync-client");
 var Pipeline = require("./lib/sync/pipeline").Pipeline;
 
 
@@ -11,7 +12,16 @@ writePipeline.addAction({
         _.each(data.syncToSrc, function(toSrc){
             var fromPath = data.trgPath + "/" + toSrc;
             var toPath = data.srcPath + "/" + toSrc;
-            fs.createReadStream(fromPath).pipe(fs.createWriteStream(toPath));
+
+            var srcHandler = sync.getHandler(fromPath);
+            var trgHandler = sync.getHandler(toPath);
+
+            srcHandler.readFile(fromPath,function(base64Data){
+               trgHandler.writeFile(toPath,base64Data,function(){
+                   console.log("Copied "+fromPath+" to "+toPath);
+               })
+            });
+
         });
         return data;
     }
@@ -21,38 +31,45 @@ writePipeline.addAction({
         _.each(data.syncToTrg, function(toTrg){
             var fromPath = data.srcPath + "/" + toTrg;
             var toPath = data.trgPath + "/" + toTrg;
-            fs.createReadStream(fromPath).pipe(fs.createWriteStream(toPath));
+
+            var srcHandler = sync.getHandler(fromPath);
+            var trgHandler = sync.getHandler(toPath);
+
+            srcHandler.readFile(fromPath,function(base64Data){
+                trgHandler.writeFile(toPath,base64Data,function(){
+                    console.log("Copied "+fromPath+" to "+toPath);
+                })
+            });
         });
         return data;
     }
 });
 
 function checkForChanges(){
-    var path1 = "./test-data/folder1";
-    var path2 = "./test-data/folder2";
+    var path1 = "dnode://folder1";
+    var path2 = "file://test-data/folder2";
 
-    var rslt = sync.compare(path1,path2,sync.filesMatchNameAndSize);
+    sync.compare(path1,path2,sync.filesMatchNameAndSize, function(rslt) {
 
-    rslt.srcPath = path1;
-    rslt.trgPath = path2;
+        rslt.srcPath = path1;
+        rslt.trgPath = path2;
 
-    return writePipeline.exec(rslt);
+        writePipeline.exec(rslt);
+    });
 }
 
 function scheduleChangeCheck(when,repeat){
     setTimeout(function(){
         console.log("Checking for changes...");
-        var rslt = checkForChanges().result;
-
-        _.each(rslt.syncToSrc, function(file){
-           console.log(file + " was sync'd to "+rslt.srcPath);
-        });
-        _.each(rslt.syncToTrg, function(file){
-            console.log(file + " was sync'd to "+rslt.trgPath);
-        });
+        checkForChanges();
 
         if(repeat){scheduleChangeCheck(when,repeat)}
     },when);
 }
 
-scheduleChangeCheck(1000,true);
+dnodeClient.connect({}, function(handler){
+    sync.fsHandlers.dnode = handler;
+    scheduleChangeCheck(1000,true);
+});
+
+
--- tests.js
+++ tests.js
@@ -1,5 +1,7 @@
 var _ = require('lodash')
 
+var uris = require("./lib/sync/dropboxuris");
+
 var test = require('./lib/test/expect').test;
 var sync = require('./lib/sync/sync');
 var Pipeline = require('./lib/sync/pipeline').Pipeline;
@@ -7,36 +9,63 @@ var Pipeline = require('./lib/sync/pipeline').Pipeline;
 var directory1 = "./test-data/folder1";
 var directory2 = "./test-data/folder2";
 
+test({name:"Dropbox URIs"}, function(tester){
+    tester.expectThat(
+        {
+            check:function(){
+                return uris.getProtocol("foo://asdf.com?asdf3=2")
+            },
+            expectedValue:"foo",
+            msg: "protocols are parsed correctly from full uris"
+        }
+    ).expectThat(
+        {
+            check:function(){
+                return uris.getProtocol("/foo/bar")
+            },
+            expectedValue:"file",
+            msg: "protocols are defaulted to file if no scheme is present in the uri"
+        }
+    )
+});
+
 test({name:"Sync Module"}, function(tester) {
     tester.expectThat(
         {
-            check: function () {
-                return sync.compare(directory1, directory2, sync.filesMatchNameAndSize).directoriesMatch();
+            checkAsync: function (cb) {
+                sync.compare(directory1, directory2, sync.filesMatchNameAndSize, function(rslt){
+                    cb(rslt.directoriesMatch());
+                });
             },
             expectedValue: false,
             msg: "Directories with files that are the same name but different sizes shouldn't match"
         }
     ).expectThat(
         {
-            check: function () {
-                return sync.compare(directory1, directory2, sync.filesMatchName).directoriesMatch();
+            checkAsync: function (cb) {
+                sync.compare(directory1, directory2, sync.filesMatchName,function(rslt){
+                    cb(rslt.directoriesMatch());
+                });
             },
             expectedValue: false,
             msg: "Directories with files that are the same name but different sizes should match"
         }
     ).expectThat(
         {
-            check: function () {
-                var rslt = sync.compare(directory1, directory2, sync.filesMatchName);
-                return rslt.syncToSrc.length === 1 && rslt.syncToSrc[0] === "test2.txt";
+            checkAsync: function (cb) {
+                sync.compare(directory1, directory2, sync.filesMatchName,function(rslt){
+                    cb(rslt.syncToSrc.length === 1 && rslt.syncToSrc[0] === "test2.txt");
+                });
             },
             expectedValue: true,
             msg: "The src should need test2.txt sync'd to it"
         }
     ).expectThat(
         {
-            check: function () {
-                return sync.compare(directory1, directory2, sync.filesMatchNameAndSize);
+            checkAsync: function (cb) {
+                sync.compare(directory1, directory2, sync.filesMatchNameAndSize, function(rslt){
+                    cb(rslt);
+                });
             },
             expectedValue: function (rslt) {
                 var hasTest = _.contains(rslt.syncToSrc, "test.txt");
@@ -46,22 +75,6 @@ test({name:"Sync Module"}, function(tester) {
                 return hasTest && hasTest2 && nothingNeedsSyncingToFolder2;
             },
             msg: "folder1 should need test2.txt and test.txt sync'd to it, but folder2 shouldn't need anything sync'd"
-        }
-    ).expectThat(
-        {
-            check:function(){
-                return sync.getProtocol("foo://asdf.com?asdf3=2")
-            },
-            expectedValue:"foo",
-            msg: "protocols are parsed correctly from full uris"
-        }
-    ).expectThat(
-        {
-            check:function(){
-                return sync.getProtocol("/foo/bar")
-            },
-            expectedValue:"file",
-            msg: "protocols are defaulted to file if no scheme is present in the uri"
         }
     )
 });
