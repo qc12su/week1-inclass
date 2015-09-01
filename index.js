528f1298d46f34518b54a7ba573df70f45b8cd7a
Jules White <jules.white@gmail.com>
2015-09-01 04:45:11 -0500

    - Added command line parameters for key configuration options - Added the ability to install the application and have dropbox   and dropbox-server commands

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+#! /usr/bin/env node
+
 var _ = require('lodash');
 var fs = require('fs');
+
+var argv = require('yargs')
+    .usage('Usage: dropbox [options]')
+    .example('dropbox --directory1 dnode://test-data/folder1 --directory2 file://test-data/folder2', '(after launching the dropbox-server) listen for beacon signals with the given receiver id and reporting websocket url')
+    .demand(['d1','d2'])
+    .alias('d1', 'directory1')
+    .nargs('d1', 1)
+    .describe('d1', 'The first directory to sync (e.g., dnode://test-data/folder1)')
+    .alias('d2', 'directory2')
+    .nargs('d2', 1)
+    .describe('d2', 'The second directory to sync (e.g., file://test-data/folder2)')
+    .describe('s', 'The sync server (defaults to 127.0.0.1)')
+    .default('s',"127.0.0.1","127.0.0.1")
+    .alias('s', 'server')
+    .nargs('s', 1)
+    .describe('p', 'The base port to connect to on the sync server')
+    .alias('p', 'port')
+    .nargs('p', 1)
+    .default('p',5004,"5004")
+    .epilog('Apache License V2 2015, Jules White')
+    .argv;
+
 
 var sync = require('./lib/sync/sync');
 var dnodeClient = require("./lib/sync/sync-client");
@@ -40,8 +64,8 @@ writePipeline.addAction({
 });
 
 function checkForChanges(){
-    var path1 = "dnode://folder1";
-    var path2 = "file://test-data/folder2";
+    var path1 = argv.directory1;
+    var path2 = argv.directory2;
 
     sync.compare(path1,path2,sync.filesMatchNameAndSize, function(rslt) {
 
@@ -60,7 +84,7 @@ function scheduleChangeCheck(when,repeat){
     },when);
 }
 
-dnodeClient.connect({}, function(handler){
+dnodeClient.connect({host:argv.server, port:argv.port}, function(handler){
     sync.fsHandlers.dnode = handler;
     scheduleChangeCheck(1000,true);
 });
